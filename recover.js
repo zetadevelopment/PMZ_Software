e2b91873c3b1458a0a3ddb51fd54b5843cae8a4c
esteban zapata <ezapata@altavistaed.com>
2019-05-13 09:23:10 -0500

    recoverpass: Mostrar alerta cuando comparación de passwords es correcta.

--- recover.js
+++ recover.js
@@ -1,23 +1,24 @@
 function validateData() {
-	document.querySelector('#validatePassword').addEventListener('submit', (e) => {
-		e.preventDefault();
-		
-		switch(document.querySelector('#inputEmail').value) {
-			case 'cajero@maxisoft.co':
-				window.location = 'recoverpass.html';
-				break;
-			default:
-				window.location = 'soon.html';
-				break;
-		};
-	});
+  document.querySelector('#validatePassword').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    switch(document.querySelector('#inputEmail').value) {
+      case 'cajero@maxisoft.co':
+        window.location = 'recoverpass.html';
+        break;
+      default:
+        window.location = 'soon.html';
+        break;
+    };
+  });
 }
 
 function changePassword() {
-	document.querySelector('#confirmPassword').addEventListener('submit', (e) => {
-		e.preventDefault();
-		
-		if (document.querySelector('#inputPassword').value === document.querySelector('#validatePassword').value) {
-		}
-	});
+  document.querySelector('#confirmPassword').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (document.querySelector('#inputPassword').value === document.querySelector('#samePassword').value) {
+      document.querySelector('#alert').classList.toggle('d-none');
+    }
+  });
 }
