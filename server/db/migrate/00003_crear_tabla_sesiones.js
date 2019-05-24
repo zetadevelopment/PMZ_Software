2c29f382b72d8ae9f4e2cae006ab1991c766eb2a
Esteban Zapata Rojas <estebanz01@outlook.com>
2019-05-24 00:55:05 -0500

    db: migrate: Crear migraciones para usuarios, roles y sesiones.
    Este cambio implementa migraciones que se cargan cuando se inicia el servidor y sólo
    se ejecutan si estas no están presentes en la base de datos.

---
+++ server/db/migrate/00003_crear_tabla_sesiones.js
@@ -0,0 +1,7 @@
+module.exports = function migration00003() {
+  this.createTable('sesiones', function() {
+    this.string('accion', { not_null: true });
+    this.integer('usuarios_id', { references: 'usuarios.id', unsigned: true });
+    this.timestamp();
+  });
+}
