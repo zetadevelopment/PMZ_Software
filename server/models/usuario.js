b1015821441b27c750c10ee3304f8d230375f5f6
Esteban Zapata Rojas <estebanz01@outlook.com>
2019-05-24 00:55:55 -0500

    models: usuario: Añadir modelo Usuario al aplicativo.
    Este cambio añade una clase inicial de Js que representa la tabla usuarios.

---
+++ server/models/usuario.js
@@ -0,0 +1,20 @@
+const Store = require('openrecord/store/mysql');
+
+class Usuario extends Store.BaseModel {
+  static definition() {
+    // validaciones
+    this.validatesPresenceOf('nombre', 'email', 'password');
+    this.validatesFormatOf('email');
+    this.validatesLengthOf('password', 8);
+
+    // relaciones
+    this.hasMany('sesiones');
+    this.hasMany('roles');
+  }
+
+  nombreCompleto() {
+    return `${this.nombre} ${this.apellido}`;
+  }
+}
+
+module.exports = Usuario;
