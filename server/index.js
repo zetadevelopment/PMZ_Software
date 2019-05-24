5f0548252cfcb8ac5f71bfeb70303e766ea34872
Esteban Zapata Rojas <estebanz01@outlook.com>
2019-05-24 00:54:15 -0500

    server: index: Conectar a base de datos maxisoft.
    Este cambio incluye las configuraciones necesarias para cargar openrecord.js como
    el ORM encargado del manejo de la base de datos.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const Store = require('openrecord/store/mysql');
 const path = require('path');
+
+const store = new Store({
+  type: 'mysql',
+  autoLoad: true,
+  database: 'maxisoft',
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  plugins: [require('openrecord/lib/base/dynamic_loading')],
+  migrations: `${__dirname}/db/migrate/*`,
+  models: `${__dirname}/models/*`
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+store.ready(async () => {
+  console.log('Conexión a base de datos completada');
+});
 
 app.use(express.static(path.join(__dirname, '/..')));
 
